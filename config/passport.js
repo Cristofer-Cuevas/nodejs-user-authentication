4f9ff6c30d22102080e42565f2a2e51f335eca97
Christopher <cristoferperezmejia80@gmail.com>
2021-11-01 14:34:28 -0400

    passport set up

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+import passport from "passport";
+// import LocalStrategy from `passport-local`.Strategy();
+import LocalStrategy from "passport-local";
+import User from "../models/User-model.js";
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+
+      const isValid = validPassword(password, user.hash, user.salt);
+
+      if (isValid) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
